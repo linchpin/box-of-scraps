18a68ed6e1c06a257db5a84739971136d1f2c601
Maxwell Morgan <mmorgan@linchpin.agency>
2021-04-02 09:49:40 -0400

    chore: Clean up PHP warnings

--- themes/box-of-scraps/includes/Linchpin/BOS_Walker_Nav_Menu.php
+++ themes/box-of-scraps/includes/Linchpin/BOS_Walker_Nav_Menu.php
@@ -24,14 +24,18 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
             $n = "\n";
         }
         $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
- 
+
         $classes   = empty( $item->classes ) ? array() : (array) $item->classes;
         $classes[] = 'menu-item-' . $item->ID;
 
-        if ( in_array( 'menu-item-has-children', $item->classes ) ) {
-        	$classes[] = 'navbar-link';
+        if ( is_array( $item->classes ) ) {
+	        if ( in_array( 'menu-item-has-children', $item->classes ) ) {
+	            $classes[] = 'navbar-link';
+	        } else {
+	            $classes[] = 'navbar-item';
+	        }
         } else {
-        	$classes[] = 'navbar-item';
+	        $classes[] = 'navbar-item';
         }
 
         $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
@@ -42,12 +46,14 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
         $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth );
         $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
 
-        if ( in_array( 'button', $item->classes ) ) {
-        	$output .= $indent . '<div class="navbar-item"><div class="buttons">';
-        } elseif ( in_array( 'menu-item-has-children', $item->classes ) ) {
-        	$output .= $indent . '<div class="navbar-item has-dropdown is-hoverable">';
+        if ( is_array( $item->classes ) ) {
+	        if ( in_array( 'button', $item->classes ) ) {
+	            $output .= $indent . '<div class="navbar-item"><div class="buttons">';
+	        } elseif ( in_array( 'menu-item-has-children', $item->classes ) ) {
+	            $output .= $indent . '<div class="navbar-item has-dropdown is-hoverable">';
+	        }
         }
- 
+
         $atts           = array();
         $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
         $atts['target'] = ! empty( $item->target ) ? $item->target : '';
@@ -60,7 +66,7 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
         $atts['aria-current'] = $item->current ? 'page' : '';
 
         $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
- 
+
         $attributes = '';
         foreach ( $atts as $attr => $value ) {
             if ( is_scalar( $value ) && '' !== $value && false !== $value ) {
@@ -68,12 +74,12 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
                 $attributes .= ' ' . $attr . '="' . $value . '"';
             }
         }
- 
+
         /** This filter is documented in wp-includes/post-template.php */
         $title = apply_filters( 'the_title', $item->title, $item->ID );
 
         $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
- 
+
         $item_output  = $args->before;
         $item_output .= '<a' . $attributes . $class_names . '>';
         $item_output .= $args->link_before . $title . $args->link_after;
@@ -104,12 +110,16 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
             $n = "\n";
         }
 
-        if ( in_array( 'button', $item->classes ) ) {
-        	$output .= "</div></div>{$n}";
-        } elseif ( in_array( 'menu-item-has-children', $item->classes ) ) {
-        	$output .= "</div>{$n}";
+        if ( is_array( $item->classes ) ) {
+	        if ( in_array( 'button', $item->classes ) ) {
+	            $output .= "</div></div>{$n}";
+	        } elseif ( in_array( 'menu-item-has-children', $item->classes ) ) {
+	            $output .= "</div>{$n}";
+	        } else {
+	            $output .= "{$n}";
+	        }
         } else {
-        	$output .= "{$n}";
+	        $output .= "{$n}";
         }
     }
 
@@ -133,13 +143,13 @@ class BOS_Walker_Nav_Menu extends Walker_Nav_Menu {
             $n = "\n";
         }
         $indent = str_repeat( $t, $depth );
- 
+
         // Default class.
         $classes = array( 'navbar-dropdown' );
 
         $class_names = implode( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
         $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
- 
+
         $output .= "{$n}{$indent}<div$class_names>{$n}";
     }
 
